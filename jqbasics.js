2c1fd76071ab2c541a803b9d54b4a7d41ba77c5c
Jeff Bowne <jeffbowne@hotmail.com>
2015-06-15 17:48:21 -0400

    Click Them All

---
+++ jqbasics.js
@@ -0,0 +1,81 @@
+jQuery(document).ready(function() {
+
+
+
+
+$(".div1").click( function() {
+	$(".div1").fadeOut(1000)
+});
+
+
+$(".div2").click( function() {
+	$(".div2").slideUp(1000)
+});
+
+$(".div3").click( function() {
+	$(".div3").animate( {
+  padding: '100',
+} , 2000 );
+});
+
+$(".div4").click( function() {
+	$(".div4").animate( {
+  marginLeft: '500',
+} , 2000 );
+});
+
+$(".div5").click( function() {
+	$(".div5").animate( {
+  fontSize: '.5em',
+} , 2000 );
+});
+
+$(".div6").click( function() {
+	$(".div6").animate( {
+  fontSize: '5em',
+} , 2000 );
+});
+
+
+$(".div7").click( function() {
+	$(".div7").animate( {
+  width: "20%",
+} , 2000 );
+});
+
+
+$(".div8").click( function() {
+	$(".div8").animate( {
+  opacity: 0.3,
+} , 2000 );
+});
+
+$(".div9").click( function() {
+	$(".div9").animate( {
+  marginLeft: '-1600px',
+} , 2000 );
+});
+
+$(".div10").click( function() {
+	$(".div10").animate( {
+  fontSize: '5em',
+} , 2000 );
+});
+
+$(".div10").click( function() {
+	$(".div10").animate( {
+  fontSize: '.5em',
+} , 2000 );
+});
+
+
+
+
+
+
+
+
+
+
+
+});
